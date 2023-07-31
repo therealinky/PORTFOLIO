8250e7be1625755ae0ea762e11c6c94d8629f1ce
Ana <106896703+therealinky@users.noreply.github.com>
2023-07-31 11:39:22 -0700

    Added images and scripts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+ function showHideDiv(ele) {
+                var srcElement = document.getElementById(ele);
+                if (srcElement != null) {
+                    if (srcElement.style.display == "block") {
+                        srcElement.style.display = 'none';
+                    }
+                    else {
+                        srcElement.style.display = 'block';
+                    }
+                    return false;
+                }
+            }
